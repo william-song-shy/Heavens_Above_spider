15ff9b7af64e1ab1a0e87b5a88c46b19b93ad7e0
william-song-shy <2560814258@qq.com>
2019-05-27 14:48:59 +0800

    Add function main

--- spider.py
+++ spider.py
@@ -7,15 +7,14 @@ def get_url (url):
     html=browser.page_source
     browser.quit()
     return str(html)
-
-num=input ()
-html=get_url('https://www.heavens-above.com/SatInfo.aspx?satid={}&'.format(num))
-html=etree.HTML(html)
-title=html.xpath('//title/text()')
-r=re.compile('(\S+) - 人造卫星信息')
-title=r.findall(title[0])
-title=title[0]
-infor=html.xpath('//div[@style="width:750px"]//text()')
-infor='\n'.join(infor)
-r=re.compile(r'([^\n\t]+)')
-infor=r.findall(infor)
+def main (num):
+    html=get_url('https://www.heavens-above.com/SatInfo.aspx?satid={}&'.format(num))
+    html=etree.HTML(html)
+    title=html.xpath('//title/text()')
+    r=re.compile('(\S+) - 人造卫星信息')
+    title=r.findall(title[0])
+    title=title[0]
+    infor=html.xpath('//div[@style="width:750px"]//text()')
+    infor='\n'.join(infor)
+    r=re.compile(r'([^\n\t]+)')
+    infor=r.findall(infor)
